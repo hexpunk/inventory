e47f11206e7047b804752664a4cbd108d83a3c64
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-11-28 00:08:50 +0000

    Update migration

--- migrations/1700954911-initial-tables.sql
+++ migrations/1700954911-initial-tables.sql
@@ -2,7 +2,10 @@ CREATE TABLE containers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
         CHECK(trim(name) != ""),
-    description TEXT NOT NULL DEFAULT ""
+    description TEXT NOT NULL DEFAULT "",
+    created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    updated_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    deleted_at TEXT
 );
 
 CREATE TABLE items (
@@ -14,5 +17,24 @@ CREATE TABLE items (
     description TEXT NOT NULL DEFAULT "",
     container_id INTEGER REFERENCES containers(id)
         ON UPDATE CASCADE
-        ON DELETE SET NULL
+        ON DELETE SET NULL,
+    created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    updated_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')),
+    deleted_at TEXT
 );
+
+CREATE INDEX items_container_id_index ON items(container_id);
+
+CREATE TRIGGER update_containers AFTER UPDATE ON containers
+    BEGIN
+        UPDATE containers
+            SET updated_at = strftime('%Y-%m-%dT%H:%M:%fZ', 'now')
+            WHERE id = new.id;
+    END;
+
+CREATE TRIGGER update_items AFTER UPDATE ON items
+    BEGIN
+        UPDATE items
+            SET updated_at = strftime('%Y-%m-%dT%H:%M:%fZ', 'now')
+            WHERE id = new.id;
+    END;
